7b963f6ef34000fa0c5d88824356db1f7425aa13
Jonathan Holvey <jonathan.holvey@outlook.com>
2019-08-16 19:59:24 +1000

    Update webpack build for production

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,40 +2,43 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const LiveReloadPlugin = require('webpack-livereload-plugin')
 const path = require('path')
 
-module.exports = {
-  watch: true,
-  mode: 'development',
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development'
+  return {
+    mode,
+    watch: mode === 'development',
 
-  entry: {
-    main: ['./src/js/main.js', './src/scss/main.scss'],
-  },
+    entry: {
+      main: ['./src/js/main.js', './src/scss/main.scss'],
+    },
 
-  output: {
-    path: path.join(__dirname, 'app/public/'),
-    filename: 'js/[name].js',
-  },
+    output: {
+      path: path.join(__dirname, 'app/public/'),
+      filename: 'js/[name].js',
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'resolve-url-loader',
-          'sass-loader',
-        ],
-      },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'resolve-url-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
+      new LiveReloadPlugin(),
     ],
-  },
-
-  plugins: [
-    new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
-    new LiveReloadPlugin(),
-  ],
+  }
 }
