2363801e539a8c230b77eac32491a3efb425ba4c
Jonathan Holvey <jonathan.holvey@outlook.com>
2019-08-16 20:36:08 +1000

    Add scroll animation to in-page links

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,4 +1,16 @@
 import Parallax from 'scroll-parallax'
+import scrollToElement from 'scroll-to-element'
 
+// Add parallax effect to header image
 const options = { intensity: 50 }
 new Parallax('.parallax', options).init()
+
+// Add scrolling effect to in-page links
+const links = document.querySelectorAll('a[href^="#"]')
+links.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault()
+    const href = event.target.getAttribute('href')
+    scrollToElement(href, { ease: 'inOutSine' })
+  })
+})
