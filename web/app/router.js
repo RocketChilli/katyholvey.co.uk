9a85974cd492d25d7ed97a0280aef65f8a33befb
Jonathan Holvey <jonathan.holvey@outlook.com>
2019-10-09 21:50:00 +1000

    Add blog route and normal page header styles

--- web/app/router.js
+++ web/app/router.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const home = require('./controllers/home')
+const blog = require('./controllers/blog')
 const mailer = require('./controllers/mailer')
 
 const router = express.Router()
 router.get('/', home)
+router.get('/blog', blog)
 router.post('/mailer', mailer)
 
 module.exports = router
