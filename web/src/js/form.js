81d82b767aa5599bd98457ebc216f001527a7c51
Jonathan Holvey <jonathan.holvey@outlook.com>
2020-08-13 19:00:52 +1000

    Convert form handler to async function

--- web/src/js/form.js
+++ web/src/js/form.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 /* Handle contact form submission
  * @param {Event} - The form submit event
  */
-const formHandler = (event) => {
+const formHandler = async (event) => {
   event.preventDefault()
   const contactForm = event.target
   contactForm.classList.add('-sending')
@@ -14,29 +14,28 @@ const formHandler = (event) => {
     email: contactForm.querySelector('[name="email"]').value,
     message: contactForm.querySelector('[name="message"]').value,
   }
-  axios({
+
+  const result = await axios({
     url: '/mailer',
     method: 'post',
     data,
     headers: { 'Content-type': 'application/json' },
   })
-    .catch(error => error)
-    .then((result) => {
-      // Get status and reset form
-      contactForm.classList.remove('-sending')
-      let status = 'error'
-      if (result.status === 200) {
-        status = 'success'
-        contactForm.reset()
-      }
 
-      // Provide feedback after sending message
-      const message = contactForm.querySelector(`.message .${status}`)
-      message.classList.add('-visible')
-      setTimeout(() => {
-        message.classList.remove('-visible')
-      }, 3000)
-    })
+  // Get status and reset form
+  contactForm.classList.remove('-sending')
+  let status = 'error'
+  if (result.status === 200) {
+    status = 'success'
+    contactForm.reset()
+  }
+
+  // Provide feedback after sending message
+  const message = contactForm.querySelector(`.message .${status}`)
+  message.classList.add('-visible')
+  setTimeout(() => {
+    message.classList.remove('-visible')
+  }, 3000)
 }
 
 export default formHandler
